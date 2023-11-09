b8e0ad9ad1fe977959531c2825f5b00cb63d5a48
dengqinzhi <toneydeng@126.com>
2023-11-09 12:11:29 +0800

    Site updated: 2023-11-09 12:11:20

---
+++ js/js.js
@@ -0,0 +1,52 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script><!--51.LA网站监控代码-->
+<script src="https://sdk.51.la/js-sdk-pro.min.js"></script>
+<script>LA.init({id: "JejfyZfHpj1jIz7S",ck: "JejfyZfHpj1jIz7S",autoTrack:true,hashMode:true})</script>
+	 <!--51.LA网站监控代码-->
+<!--51.LA应用监控代码-->
+<script>
+  !(function(c,i,e,b){
+  var h=i.createElement("script");
+  var f=i.getElementsByTagName("script")[0];
+  h.type="text/javascript";
+  h.crossorigin=true;
+  h.onload=function(){new c[b]["Monitor"]().init({id:"JejhPVjaXiV98JKA",sendSuspicious:true,sendSpaPv:true});};
+  f.parentNode.insertBefore(h,f);h.src=e;})(window,document,"https://sdk.51.la/perf/js-sdk-perf.min.js","LingQue");
+</script>
+<!--51.LA应用监控代码-->
+<!--百度统计代码-->
+<!--qinzhi.cc-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1234405b732eec2f096698022565a092";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<!--qinzhi.cc-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c707cd5bbe41c3b906e3745dc45c3ff5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+<!--百度统计代码-->
+<!--今日头条推送-->
+<script>
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?4a4af92d849e0e9ee3e96bd737d3beb31a0350ac2bedc90c5817a2542f0923964a4d40de0dd5fa9b5c2c10f69a3b501dc430e831103b45ce33654fb9f95b006c";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
+</script>
+<!--今日头条推送-->
+
+
+<script async src="https://stats.qinzhi.cc/tongji" data-website-id="b677d8cd-8ee8-4616-947c-800f5d35da71"></script>
